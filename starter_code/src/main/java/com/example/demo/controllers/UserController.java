2824b4130b49120af4bb039c8ba2ffcd9ddf5359
9gkr <135762112+9gkr@users.noreply.github.com>
2023-08-04 11:44:35 -0700

    Update UserController.java
    Add password check and logging

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,8 @@ import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -28,14 +33,27 @@ public class UserController {
 	@Autowired
 	private CartRepository cartRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	private final static Logger log = getLogger(UserController.class);
+
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		if (!userRepository.findById(id).isPresent()){
+			// add log for null user
+			log.error("UserController: Failed to find user with id {}", id);
+		}
 		return ResponseEntity.of(userRepository.findById(id));
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
+		if (user == null){
+			// add log for null user
+			log.error("UserController: Failed to find user with username {}", username);
+		}
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
 	
@@ -46,7 +64,32 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
-		userRepository.save(user);
+
+		// make sure that the password length is at least 7 and password equals to confirmPassword
+		if (createUserRequest.getPassword().length() < 7
+				|| !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			// add log for create user request failures and exceptions
+			log.error("UserController: Failed to set password for {}", createUserRequest.getUsername());
+			log.info("UserController: Create user failure");
+			return ResponseEntity.badRequest().build();
+		}
+		
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		
+		try{
+			userRepository.save(user);
+		}
+		
+		// catch exception if username unique constraint is violated
+        	catch (DataIntegrityViolationException e){
+			// add log for CreateUser request failure
+			log.error("UserController: Failed to set unique username for {}", createUserRequest.getUsername());
+			log.error("UserController: Create user failure");
+            		System.out.println(e.getMessage());
+			return ResponseEntity.badRequest().build();
+        	}
+		// add log for CreateUser request successes
+		log.info("UserController: Create user success");
 		return ResponseEntity.ok(user);
 	}
 	
